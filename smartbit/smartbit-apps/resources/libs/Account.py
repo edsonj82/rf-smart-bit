b10dbfea3c1b92a141cf5233be9db5e892b23019
edsonj82 <edsonj82@outlook.com>
2024-09-20 19:27:38 -0300

    Implemented Lib Faker

--- smartbit/smartbit-apps/resources/libs/Account.py
+++ smartbit/smartbit-apps/resources/libs/Account.py
@@ -1,9 +1,11 @@
+from faker import Faker
 
+fake = Faker('pt_BR')
 
 def get_fake_account():
     account ={
-    "name": "Edson José dos Santos",
-    "email": "edson@xpto.com",
-    "document": "38297527093"
+    "name": fake.name(),
+    "email": fake.email(),
+    "document": fake.cpf()
     }
     return account
